ecb3c6fc11adf7cd3e8a605442c8ade1527a5439
aoperez <aoperez@muniguate.com>
2021-03-18 11:01:20 -0600

    new updates

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,6 +1,7 @@
 const util = require("util");
 const multer = require("multer");
 const maxSize = 2 * 1024 * 1024;
+// const fileType = "json";
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -15,6 +16,13 @@ let storage = multer.diskStorage({
 let uploadFile = multer({
   storage: storage,
   limits: { fileSize: maxSize },
+  fileFilter: function (req, file, cb) {
+    if (path.extension(file.originalname) !== '.json') {
+        // return cb(null, false);
+        cb("Error: only png, jpeg, and jpg are allowed!");
+    }
+    cb(null, true)
+  }
 }).single("file");
 
 let uploadFileMiddleware = util.promisify(uploadFile);
